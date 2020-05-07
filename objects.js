7ebc76117c7252b12516bd1d172a98e991930603
Nick Spangler <spangleyman@mac.com>
2020-05-07 20:21:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {"Queen": "I Want It All"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist = playlist.artistName = songTitle;
   return playlist;
 }
 updatePlaylist(playlist, "Phil Ochs", "My Bloody Valentine");
